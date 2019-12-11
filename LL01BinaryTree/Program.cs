cfea25be91b490174e2b6f30ca3eb64af94eca1f
Michal Barth <u17.401@pslib.cloud>
2019-12-11 09:35:12 +0100

    11.12

---
+++ LL01BinaryTree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LL01BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBinaryTree<DictionaryTerm> tree = new MyBinaryTree<DictionaryTerm>();
+            tree.Add(new DictionaryTerm("pes", "dog"));
+            tree.Add(new DictionaryTerm("kočka", "cat"));
+            tree.Add(new DictionaryTerm("listí", "leaves"));
+            TreeNode<DictionaryTerm> found = tree.Find(new DictionaryTerm("kočka", ""));
+            if (found != null) Console.WriteLine(found.value.English);
+            Console.WriteLine(tree);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LL01BinaryTree/TreeNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LL01BinaryTree
+{
+    class TreeNode<T> where T: IComparable
+    {
+        internal T value;
+
+        public TreeNode(T value)
+        {
+            this.value = value;
+        }
+
+        internal TreeNode<T> Left;
+        internal TreeNode<T> Right;
+    }
+}
